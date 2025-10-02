6d4eece9ce834239124b2c86b32bba454fcdf274
tomatoweb <math.appelmans@gmail.com>
2025-10-02 12:21:59 +0200

    prepare merge

--- dotdev/insert_to_db.php
+++ dotdev/insert_to_db.php
@@ -1,5 +1,6 @@
 <?php
 
+//$servername = "172.17.0.3"; // le nom de serveur a utiliser sans docker-compose.yml
 $servername = "mysql"; // le nom du service dans docker-compose.yml (le container php et le container mysql partage le même réseau et peuvent donc communiquer via leur nom de service/container)
 $username = "root";
 $password = "pw";
@@ -11,8 +12,8 @@ if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
 }
 
-//$sql = "INSERT INTO example_table (first_name, last_name) VALUES ('John', 'Doe'), ('Jane', 'Doe')";
-$sql = "INSERT INTO example_table (first_name, last_name) VALUES ('Jack', 'Russel')";
+$sql = "INSERT INTO example_table (first_name, last_name) VALUES ('John', 'Doe'), ('Jane', 'Doe')";
+//$sql = "INSERT INTO example_table (first_name, last_name) VALUES ('Jack', 'Russel')";
 
 if ($conn->query($sql) === TRUE) {
   echo "New records created successfully";
